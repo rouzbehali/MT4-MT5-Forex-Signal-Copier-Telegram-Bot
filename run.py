d8a1f796175875faaed717c20bc1bc278feb53bf
rouzbehali <46296887+rouzbehali@users.noreply.github.com>
2023-10-26 03:13:58 +0330

    Update run.py

--- run.py
+++ run.py
@@ -91,17 +91,7 @@ def ParseSignal(signal: str) -> dict:
 
 
     
-    # calculates the position size using stop loss and RISK FACTOR
-    if('Lot'.lower() in signal[1].lower()):
-        trade['RiskFactor'] = 1
-        trade['PositionSize'] = float((signal[1].split())[-1])
-
-    # adds risk factor to trade
-    elif('Risk'.lower() in signal[1].lower()):
-        trade['RiskFactor'] = float((signal[1].split())[-1])
-        trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']) / stopLossPips) / 10 * 100) / 100
-    else:
-        return {}
+    
 
 
 
@@ -156,6 +146,18 @@ def GetTradeInformation(update: Update, trade: dict, balance: float) -> None:
 
     # calculates the stop loss in pips
     stopLossPips = abs(round((trade['StopLoss'] - trade['Entry']) / multiplier))
+    
+    # calculates the position size using stop loss and RISK FACTOR
+    if('Lot'.lower() in signal[1].lower()):
+        trade['RiskFactor'] = 1
+        trade['PositionSize'] = float((signal[1].split())[-1])
+
+    # adds risk factor to trade
+    elif('Risk'.lower() in signal[1].lower()):
+        trade['RiskFactor'] = float((signal[1].split())[-1])
+        trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']) / stopLossPips) / 10 * 100) / 100
+    else:
+        return {}
 
     # calculates the take profit(s) in pips
     takeProfitPips = []
